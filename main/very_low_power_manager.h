9677a1fe4ac44159d370c76b03af5aa3dfcaa426
Joona <77233664+controlengineering-ch@users.noreply.github.com>
2025-08-10 16:04:20 +0200

    Remove unused include in low power manager

--- main/very_low_power_manager.h
+++ main/very_low_power_manager.h
@@ -9,12 +9,15 @@
 extern "C" {
 #endif
 
-**_void init_power_management();_**
-**_void enter_deep_sleep_with_timer(uint64_t sleep_us);_**
-**_void enable_gpio_wakeup(gpio_num_t pin);_**
-**_void enable_both_wakeups(gpio_num_t pin, uint64_t sleep_us);_**
-**_bool was_wakeup_from_gpio();_**
-**_void print_wakeup_reason();_**
+void init_power_management();
+void enter_deep_sleep_with_timer(uint64_t sleep_us);
+void enable_gpio_wakeup(gpio_num_t pin);
+void enable_both_wakeups(gpio_num_t pin, uint64_t sleep_us);
+bool was_wakeup_from_gpio();
+void print_wakeup_reason();
+bool should_perform_ble_scan(uint64_t interval_min);
+bool should_attempt_wifi_upload(uint64_t interval_min);
+void prepare_for_deep_sleep(uint64_t sleep_us, gpio_num_t wake_gpio);
 
 #ifdef __cplusplus
 }
